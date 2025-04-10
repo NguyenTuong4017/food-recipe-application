59b985a1b98e295fdce2baa6ad48975bbc88de55
NguyenTuong0420 <manhtuonga2bd@Gmail.com>
2025-04-10 15:56:37 +0300

    Arrange the code to make it more logical. Add some comments

--- Components/Body/Body.tsx
+++ Components/Body/Body.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect, useRef } from "react";
 import FilterButtons from "./BodyComponents/MainComponents/FilterButtons";
 
 import FoodMenu from "./BodyComponents/MainComponents/RecipeMenu";
+import LoadingScreen from "../../JavaScriptFiles/LoadingScreen";
 import LottieView from "lottie-react-native";
 
 interface BodyProps {
@@ -11,10 +12,38 @@ interface BodyProps {
 }
 
 export default function Body({ recipeFromSearchBar }: BodyProps) {
-  const [allRecipes, setAllRecipes] = useState<Array<Object>>([]);
-  const [isLoading, setLoading] = useState<boolean>(false);
-  const animationRef = useRef<LottieView>(null);
+  const [allRecipes, setAllRecipes] = useState<Array<Object>>([]); //Recipe get from filter buttons
 
+  const [isLoading, setLoading] = useState<boolean>(true); //Loading Status
+  const animationRef = useRef<LottieView>(null); //Ref for controlling the Lottie animations
+
+  //recipeFromSearchBar's objects doesn't have the index property, this is an update with index property
+  const [recipeFromSearchWithIndex, setRecipeFromSearchWithIndex] = useState<
+    Array<{ index: number }>
+  >([]);
+
+  //Because the objects in the recipeFromSearchBar doesn't have index property, this function adds the index to them.
+  const setIndexForRecipeFromSearchBar = () => {
+    const updatedRecipes = recipeFromSearchBar.map((recipe, index) => ({
+      ...recipe,
+      index: index,
+    }));
+
+    setRecipeFromSearchWithIndex(updatedRecipes);
+  };
+
+  //Get the recipes from filter buttons
+  const handleRecipesUpdate = (recipes: Array<Object>) => {
+    setAllRecipes(recipes);
+    replayAnimation();
+  };
+
+  //Reset the recipeFromSearchBar to empty when user clicks a filter button.
+  const resetRecipeFromSearch = () => {
+    setRecipeFromSearchWithIndex([]);
+  };
+
+  //Restart the loading animation
   const replayAnimation = () => {
     if (animationRef.current) {
       animationRef.current.reset();
@@ -22,33 +51,9 @@ export default function Body({ recipeFromSearchBar }: BodyProps) {
     }
   };
 
-  const [recipeFromSearch, setRecipeFromSearch] = useState<
-    Array<{ index: number }>
-  >([]);
-
-  const handleRecipesUpdate = (recipes: Array<Object>) => {
-    setAllRecipes(recipes);
-  };
-
   //Handle the loading progress
   const handleLoading = (loading: boolean) => {
     setLoading(loading);
-    replayAnimation();
-  };
-
-  //Because the object in the recipeFromSearchBar doesn't have index property, this function adds the index to them.
-  const setIndexForRecipeFromSearchBar = () => {
-    const updatedRecipes = recipeFromSearchBar.map((recipe, index) => ({
-      ...recipe,
-      index: index,
-    }));
-
-    setRecipeFromSearch(updatedRecipes);
-  };
-
-  //Reset the recipeFromSearchBar to empty when user clicks a filter button.
-  const resetRecipeFromSearch = () => {
-    setRecipeFromSearch([]);
   };
 
   useEffect(() => {
@@ -73,20 +78,15 @@ export default function Body({ recipeFromSearchBar }: BodyProps) {
         resetSearch={resetRecipeFromSearch}
       />
 
+      {/* if the recipeFromSearch is existed then show it on screen otherwise show the recipes from filter buttons */}
       {isLoading ? (
-        <View style={styles.loadingScreen}>
-          <LottieView
-            ref={animationRef}
-            source={require("../../assets/animations/circleLoading.json")}
-            autoPlay
-            loop={false}
-            style={{ width: 100, height: 100 }}
-          />
-        </View>
+        <LoadingScreen animationRef={animationRef} />
       ) : (
         <FoodMenu
           allRecipes={
-            recipeFromSearch.length > 0 ? recipeFromSearch : allRecipes
+            recipeFromSearchWithIndex.length > 0
+              ? recipeFromSearchWithIndex
+              : allRecipes
           }
         />
       )}
@@ -101,13 +101,5 @@ const styles = StyleSheet.create({
     height: "70%",
     bottom: "2.5%",
     //backgroundColor: "purple",
-  },
-
-  loadingScreen: {
-    alignItems: "center",
-    justifyContent: "center",
-    //backgroundColor: "red",
-    width: "100%",
-    height: "90%",
   },
 });
