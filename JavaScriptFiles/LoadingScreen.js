b88cb76f2635e8fa84e0d959b4bba38d6d2dd5c3
NguyenTuong0420 <manhtuonga2bd@Gmail.com>
2025-04-10 15:56:02 +0300

    update the LoadingScreen.js

--- JavaScriptFiles/LoadingScreen.js
+++ JavaScriptFiles/LoadingScreen.js
@@ -1,1 +1,23 @@
-export default function LoadingScreen() {}
+import LottieView from "lottie-react-native";
+import { StyleSheet, View } from "react-native";
+export default function LoadingScreen({ animationRef }) {
+  return (
+    <View style={styles.loadingScreen}>
+      <LottieView
+        ref={animationRef}
+        source={require("../assets/animations/circleLoading.json")}
+        loop={false}
+        style={{ width: 100, height: 100 }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  loadingScreen: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "90%",
+  },
+});
