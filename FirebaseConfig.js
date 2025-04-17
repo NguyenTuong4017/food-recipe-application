a0aa8ca34728cfe8752262fa40f3e66bf6b360e3
NguyenTuong0420 <manhtuonga2bd@Gmail.com>
2025-04-17 18:20:23 +0300

    add db for saving function

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { FIREBASE_API_KEY } from "@env";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,4 +26,5 @@ const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
 
-export default auth;
+const db = getFirestore(app);
+export { auth, db };
